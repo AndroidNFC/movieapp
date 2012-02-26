3c0c1e9d54b948efe0eabe67d538f83797bfce6b
Tatu Paronen <tatu.paronen@gmail.com>
2012-02-26 14:11:03 +0200

    first commit

---
+++ src/androidnfc/movieapp/MovieappActivity.java
@@ -0,0 +1,13 @@
+package androidnfc.movieapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MovieappActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
