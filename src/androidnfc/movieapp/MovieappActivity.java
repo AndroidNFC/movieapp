ed9a376d2bb6e92c4564a100c6384c7dc4c4f260
Ken <wsd.2012.ken@gmail.com>
2012-03-07 15:58:22 +0200

    Added buttons.

--- src/androidnfc/movieapp/MovieappActivity.java
+++ src/androidnfc/movieapp/MovieappActivity.java
@@ -2,12 +2,16 @@ package androidnfc.movieapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MovieappActivity extends Activity {
     /** Called when the activity is first created. */
+	private Button nfcButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //nfcButton = (Button)findViewById(R.id.nfcTagButton);
     }
 }
