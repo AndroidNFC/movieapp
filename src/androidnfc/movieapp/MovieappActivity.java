e3a28e8418a61d7e6fce13fc9faa16df29d3f481
Ken <wsd.2012.ken@gmail.com>
2012-03-07 17:05:03 +0200

    open browser event

--- src/androidnfc/movieapp/MovieappActivity.java
+++ src/androidnfc/movieapp/MovieappActivity.java
@@ -1,9 +1,14 @@
 package androidnfc.movieapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MovieappActivity extends Activity {
     /** Called when the activity is first created. */
@@ -17,5 +22,20 @@ public class MovieappActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         nfcTagButton = (Button)findViewById(R.id.nfcTagButton);
+        openBrowser = (Button)findViewById(R.id.openBrowser);
+        openBrowser.setOnClickListener(new openBrowserOnClickListener());
     }
+    
+    private final class openBrowserOnClickListener implements OnClickListener {
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Toast toast = Toast.makeText(getApplicationContext(), "open browser", Toast.LENGTH_SHORT);
+			toast.setGravity(Gravity.CENTER, 0, 0);
+			toast.show();
+//			Intent it = new Intent();
+//			it.putExtra("CLocation", getReceive());
+//			it.setClass(Selecao.this, N3SeleText.class);
+//			startActivity(it);
+		}
+}
 }
