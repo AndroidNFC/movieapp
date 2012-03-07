762f24091ea6ea0be73235e069495aa7504fdc1a
Ken <wsd.2012.ken@gmail.com>
2012-03-07 16:12:35 +0200

    add buttons and fix bugs

--- gen/androidnfc/movieapp/R.java
+++ gen/androidnfc/movieapp/R.java
@@ -14,13 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int testView=0x7f050000;
+        public static final int nfcTagButton=0x7f050001;
+        public static final int openBrowser=0x7f050003;
+        public static final int openMap=0x7f050004;
+        public static final int openVideo=0x7f050005;
+        public static final int textView=0x7f050000;
+        public static final int xmlParserButton=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int browserbutton=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int nfcTagbutton=0x7f040002;
+        public static final int openMap=0x7f040005;
+        public static final int openVideo=0x7f040006;
+        public static final int xmlbutton=0x7f040003;
     }
 }
--- src/androidnfc/movieapp/MovieappActivity.java
+++ src/androidnfc/movieapp/MovieappActivity.java
@@ -3,15 +3,15 @@ package androidnfc.movieapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MovieappActivity extends Activity {
     /** Called when the activity is first created. */
-	private Button nfcButton;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        //nfcButton = (Button)findViewById(R.id.nfcTagButton);
+        Button nfcButton = (Button)findViewById(R.id.nfcTagButton);
     }
 }
