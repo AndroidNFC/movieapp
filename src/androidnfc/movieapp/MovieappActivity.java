c43b1f009ebb8e979254e2875b922e953b587146
Tatu Paronen <tatu.paronen@gmail.com>
2012-03-08 14:11:36 +0200

    Added new activity for development of xml parsing code.

--- src/androidnfc/movieapp/MovieappActivity.java
+++ src/androidnfc/movieapp/MovieappActivity.java
@@ -1,23 +1,37 @@
 package androidnfc.movieapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MovieappActivity extends Activity {
-    /** Called when the activity is first created. */
 	
 	private Button nfcTagButton;
 	private Button xmlParserButton;
-	private Button openBrowser;
-	private Button openMap;
-	private Button openVideo;
+	private Button openBrowserButton;
+	private Button openMapButton;
+	private Button openVideoButton;
 
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        nfcTagButton = (Button)findViewById(R.id.nfcTagButton);
+        
+        nfcTagButton =		(Button)findViewById(R.id.nfcTagButton);
+        xmlParserButton = 	(Button)findViewById(R.id.xmlParserButton);
+        openBrowserButton = (Button)findViewById(R.id.openBrowserButton);
+        openMapButton = 	(Button)findViewById(R.id.openMapButton);
+        openVideoButton = 	(Button)findViewById(R.id.openVideoButton);
+        
+        xmlParserButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MovieappActivity.this,
+										   XMLParserActivity.class);
+				MovieappActivity.this.startActivity(intent);
+			}
+		});
     }
 }
