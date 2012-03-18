12be39f7030310901abda3cb68d294fa59d4f4f8
Mikko Vesikkala <vokkim@gmail.com>
2012-03-18 15:58:11 +0200

    Some layout stuff

--- gen/androidnfc/movieapp/R.java
+++ gen/androidnfc/movieapp/R.java
@@ -12,16 +12,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int navigation_back=0x7f020001;
+        public static final int search=0x7f020002;
+        public static final int topbar_back=0x7f020003;
     }
     public static final class id {
-        public static final int nfcTagButton=0x7f050000;
-        public static final int openBrowserButton=0x7f050002;
-        public static final int openMapButton=0x7f050003;
-        public static final int openVideoButton=0x7f050004;
-        public static final int xmlParserButton=0x7f050001;
+        public static final int appname=0x7f06000c;
+        public static final int linearLayout1=0x7f060006;
+        public static final int linearLayout2=0x7f060007;
+        public static final int nfcTagButton=0x7f060001;
+        public static final int openBrowserButton=0x7f060003;
+        public static final int openMapButton=0x7f060004;
+        public static final int openVideoButton=0x7f060005;
+        public static final int search=0x7f060009;
+        public static final int searchfield=0x7f060008;
+        public static final int textView1=0x7f06000a;
+        public static final int topbar=0x7f060000;
+        public static final int topbar_back=0x7f06000b;
+        public static final int xmlParserButton=0x7f060002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int search=0x7f030001;
+        public static final int topbar=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -31,5 +44,15 @@ public final class R {
         public static final int openMap=0x7f040005;
         public static final int openVideo=0x7f040006;
         public static final int xmlbutton=0x7f040003;
+    }
+    public static final class style {
+        public static final int ContentLayout=0x7f050006;
+        public static final int MovieappTheme=0x7f050000;
+        public static final int TopBar=0x7f050002;
+        public static final int TopBar_appname=0x7f050004;
+        public static final int TopBar_back=0x7f050003;
+        public static final int TopBar_search=0x7f050005;
+        public static final int h1=0x7f050001;
+        public static final int searchfield=0x7f050007;
     }
 }
